c12e74157b7e525f07af423f7b5d929a9abe5f82
nurmukhamed <nurkamalnurmukhamed@gmail.com>
2023-02-20 18:07:33 +0600

    feat: added AuthController and register function

---
+++ Project/backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'firstname' => $request->input('firstname'),
+            'lastname' => $request->input('lastname'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response(new UserResource($user), Response::HTTP_CREATED);
+    }
+}
