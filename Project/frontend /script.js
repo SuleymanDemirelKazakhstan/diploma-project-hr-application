2ca3e74660f08ce962f281d5f9e2dac5a6d0fd73
sturzhanova <sturzhanova.gmail.com>
2023-02-23 22:44:33 +0600

    initial commit

---
+++ Project/frontend /script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container"),
+  showPassword = document.querySelectorAll(".eye-icon "),
+  pwFields = document.querySelectorAll(".password"),
+  signUp = document.querySelector(".signup-link"),
+  login = document.querySelector(".login-link");
+login.innerHTML = "test";
+console.log(login.innerHTML);
+
+// js code to show/hide password
+signUp.addEventListener("click", () => {
+  container.classList.add("active");
+});
+login.addEventListener("click", () => {
+  container.classList.remove("active");
+});
